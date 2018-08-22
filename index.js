44f3756218c19377e5e4fe16cf7248adcc3a3637
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-22 13:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+function appendDriver(name){
+  const appendDriver = [...drivers,name];
+}
+function prependDriver(name){
+  const prependDriver = [name,...drivers];
+}
+function removeLastDriver(){
+  return drivers.slice(-1);
+}
+function removeFirstDriver(){
+  return drivers.slice(1);
+}
